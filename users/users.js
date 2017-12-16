7a533851f51810ffee0af5371e2d616e30703717
Shaban Ramadani <shaban.ramadani@comm-it.it>
2017-12-16 13:12:36 +0100

    users crud

--- users/users.js
+++ users/users.js
@@ -20,10 +20,49 @@ angular.module('users').config(function ($stateProvider) {
         }
     });
 
-}).controller('usersController', function ($log, users) {
+}).controller('usersController', function ($log, users, $http, $uibModal, $filter) {
     $log.info('User controller is now loaded');
     var vm = this;
     vm.title = 'Users';
     vm.users = users;
+
+    function internalSearch () {
+        $http.get('http://localhost:8000/api/v1/users').then(function (response) {
+            vm.users = response.data.data;
+        });
+    }
+
+    function openUserModal (user) {
+        return $uibModal.open({
+            templateUrl: 'modals/userModal/userModal.html',
+            controller: 'userModalController',
+            controllerAs: 'user',
+            resolve: {
+                user: function () {
+                    return angular.copy(user);
+                }
+            }
+        });
+    }
+
+    vm.createNewUser = function () {
+        openUserModal().result.then(function () {
+            internalSearch();
+        });
+    };
+
+    vm.editUser = function (user) {
+        openUserModal(user).result.then(function () {
+            internalSearch();
+        });
+    };
+
+    vm.deleteUser = function (id) {
+        $http.delete('http://localhost:8000/api/v1/users/' + id).then(function () {
+            internalSearch();
+        });
+    };
+
+
 });
 
